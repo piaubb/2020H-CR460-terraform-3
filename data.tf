9de89d27fd3b59ad2d4463cf92b2605834e30926
piaubb <60200928+piaubb@users.noreply.github.com>
2020-02-11 15:48:25 -0500

    first commit

--- data.tf
+++ data.tf
@@ -1,8 +1,6 @@
-/*data "google_project" "project" {
-}*/
 data "google_compute_network" "default-network" {
   name = "default"
-  project = data.google_project.project.name
+  #project = data.google_project.project.name
 }
 
 data "google_project" "project" {
@@ -11,5 +9,5 @@ data "google_project" "project" {
 
 data "google_compute_network" "cr460demo" {
   name = "cr460demo"
-  project = data.google_project.project.name
+  #project = data.google_project.project.name
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "project_id" {
+variable "project_id"{
   type = string
-  default = "zonedetest"
+  default = "cr-460-gettingstartedterraform"           
 }
 
 variable "zone"{
